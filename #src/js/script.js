49f2f57fb0d232306cb0e1c7e5dd21d6e3433c34
DuremarDuremar <duremar98@yandex,ru>
2020-05-17 23:32:12 +0300

    next

--- #src/js/script.js
+++ #src/js/script.js
@@ -9,33 +9,87 @@ let imgHome = document.querySelectorAll(".content-home-portfolio-block img");
 let imgHomePort = document.querySelector(".content-home-portfolio-block");
 let modalW = document.querySelector(".modal-window");
 // console.log(modalW);
+let test11 = document.querySelector(".test11");
+let test12 = document.querySelector(".test12");
+// console.log(test11);
+
+// for (let i = 0; i < imgHome.length; i++) {
+//   console.log(imgHome[3]);
+// }
+
+for (let item of imgHome) {
+  console.log(item);
+}
 
 imgHome.forEach((e) => {
-  e.addEventListener("click", function () {
+  e = 2;
+  console.log(imgHome.index);
+
+  imgHome[e].addEventListener("click", function () {
     document.documentElement.scrollTop = 0;
-    e.classList.add("active");
+    imgHome[e].classList.add("active");
     modalW.classList.remove("modal-windowOff");
     document.body.style.overflow = "hidden";
+
+    /////////////////////////open///////////////////
+    // let activ = document.querySelector(".active");
+    // console.log(activ);
+    // document.querySelector(".modal-right").onclick = function () {
+    //   activ.classList.remove("active");
+    // };
+
+    document
+      .querySelector(".modal-right")
+      .addEventListener("click", function () {
+        imgHome[e].classList.remove("active");
+        if (e + 1 == imgHome.length) {
+          e = 0;
+        } else {
+          e++;
+        }
+        imgHome[e].classList.add("active");
+      });
+    document
+      .querySelector(".modal-left")
+      .addEventListener("click", function () {
+        // e = e;
+        imgHome[e].classList.add("active");
+      });
+    //   imgHome[activ].classList.remove("active");
+    //   console.log(imgHome[activ]);
+    //   if (activ + 1 == imgHome.length) {
+    //     activ = 0;
+    //   } else {
+    //     activ++;
+    //   }
+
+    /////////////////////close//////////////////
     document
       .querySelector(".modal-close")
       .addEventListener("click", function () {
         modalW.classList.add("modal-windowOff");
-        e.classList.remove("active");
+
+        imgHome[e].classList.remove("active");
         document.body.style.overflow = "";
       });
   });
 });
 
-let activ = document.querySelector(".active");
-console.log(activ);
+// let activ = document.querySelector(".active");
+// console.log(activ);
+
+// document.querySelector(".modal-right").onclick = function () {
+//   activ.classList.remove("active");
+// };
 
 // let activ = 0;
 
 // document.querySelector(".modal-right").onclick = function () {
-//     images[activ].classList.remove("active");
-//     console.log(images[activ]);
-//     if (activ + 1 == images.length) {
-//       activ = 0;
-//     } else {
-//       activ++;
-//     }
+//   images[activ].classList.remove("active");
+//   console.log(images[activ]);
+//   if (activ + 1 == images.length) {
+//     activ = 0;
+//   } else {
+//     activ++;
+//   }
+// };
